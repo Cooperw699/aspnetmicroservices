4d33657943f6dfbf812db30e0cd39d78c2a9bf75
Tu <lbtu@yahoo.com>
2024-01-26 21:58:04 -0700

    service layer

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,20 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var connectionString = configuration.GetValue<string>("DatabaseSettings:ConnectionString");
+            var client = new MongoClient(connectionString);
+            var dbName = configuration.GetValue<string>("DatabaseSettings:DatabaseName");
+            var database = client.GetDatabase(dbName);
+            var tableName = configuration.GetValue<string>("DatabaseSettings:CollectionName");
+            Products = database.GetCollection<Product>(tableName);
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
